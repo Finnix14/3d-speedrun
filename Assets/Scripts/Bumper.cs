27a8a9a490e1785d253787724675c3eb0f4bd21a
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2022-04-04 01:02:13 +0100

    Mushroom Level 2 now has particle system and biunces

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -6,10 +6,19 @@ public class Bumper : MonoBehaviour
 {
     [SerializeField] string playerTag;
     [SerializeField] float bounceForce;
+
+    public Animator anim;
+    public ParticleSystem bounceclouds;
+    void Start()
+    {
+        anim = GetComponent<Animator>();  
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == playerTag)
         {
+            anim.SetTrigger("doesCollide");
+            bounceclouds.Play();
             Rigidbody otherRB = collision.rigidbody;
 
             otherRB.AddExplosionForce(bounceForce, collision.contacts[0].point, 5);
