fb44f1d563012224549b1ff6182e07822dce5232
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2022-05-18 18:25:23 +0100

    Added Start Lines

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -7,15 +7,15 @@ public class NextLevel : MonoBehaviour
 {
     public void Level1()
     {
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("Level 2");
     }
     public void Level2()
     {
-        SceneManager.LoadScene("Level 2");
+        SceneManager.LoadScene("Level 3");
     }
     public void Level3()
     {
-        SceneManager.LoadScene("Level 3");
+        SceneManager.LoadScene("Level 4");
     }
     public void CutScene()
     {
